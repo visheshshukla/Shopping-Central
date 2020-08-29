dfdebb940c80c9b83de1948a508a19c907f0f8b9
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-08-29 09:01:43 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-server.listen(3000);
+app.listen(3000);

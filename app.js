370f057c36da222a42c48e12865c2fc68da6be4c
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-09-04 14:05:08 +0530

    added login form

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -40,8 +42,8 @@ mongoose
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'vishesh',
-          email: 'vishesh@test.com',
+          name: 'test',
+          email: 'test@test.com',
           cart: {
             items: []
           }

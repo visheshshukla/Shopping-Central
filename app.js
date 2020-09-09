e3d7703e5138f90598ef72278939a7ee5dff5600
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-09-09 17:53:25 +0530

    added file upload

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = process.env.MONGO
+const MONGODB_URI = process.env.MONGO;
 
 const app = express();
 const store = new MongoDBStore({
@@ -19,6 +20,15 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -28,6 +38,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ storage: fileStorage }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
